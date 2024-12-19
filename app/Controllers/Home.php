12ae9e0ac25f845e88d75eaa9aac5bbec3758685
Tom Papatolis <49147550+tompapatolis@users.noreply.github.com>
2024-12-19 20:51:48 +0100

    Initial Config

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,8 @@
-<?php
+<?php namespace App\Controllers;
 
-namespace App\Controllers;
+class Home extends BaseController {
 
-class Home extends BaseController
-{
-    public function index(): string
-    {
+    public function index(): string {
         return view('welcome_message');
     }
 }
